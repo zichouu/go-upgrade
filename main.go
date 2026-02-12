d76093db81ae38609756e6d3fa497edd8cf1c981
zichou <1303037989@qq.com>
2026-02-13 06:58:20 +0800

    first

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("hello")
+	filepath.WalkDir("../test", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			// 跳过某些目录
+			if d.Name() == ".git" || d.Name() == "node_modules" {
+				return filepath.SkipDir
+			}
+			upGrade(path)
+		}
+		return nil
+	})
 }
+
+func upGrade(path string) {
+	fmt.Println(colorPurple, path, "尝试目录", colorReset)
+	// git pull
+	joinGit := filepath.Join(path, ".git")
+	if isExist(joinGit) {
+		fmt.Println(colorBlue, path, "git pull 执行", colorReset)
+		runCmd(path, "git", "pull")
+	}
+	// pnpm i
+	joinPnpm := filepath.Join(path, "pnpm-lock.yaml")
+	if isExist(joinPnpm) {
+		fmt.Println(colorBlue, path, "pnpm i 执行", colorReset)
+		runCmd(path, "pnpm", "i")
+	}
+}
+
+func runCmd(dir, name, args string) {
+	cmd := exec.Command(name, args)
+	cmd.Dir = dir
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(colorGreen, dir, name, args, "完成", colorReset)
+	fmt.Println(string(out))
+}
+
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+var colorReset = "\033[0m"
+var colorGreen = "\033[30;42m"
+var colorBlue = "\033[97;44m"
+var colorPurple = "\033[97;45m"
