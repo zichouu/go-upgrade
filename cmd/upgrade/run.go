38eeeaa3b38195017721fc91adb3100affb11f3c
zichou <1303037989@qq.com>
2026-02-15 06:36:24 +0800

    不适合并发

--- cmd/upgrade/run.go
+++ cmd/upgrade/run.go
@@ -5,28 +5,18 @@ import (
 
 	"github.com/zichouu/go-upgrade/pkg/color"
 	"github.com/zichouu/go-upgrade/pkg/execpint"
-	"golang.org/x/sync/errgroup"
 )
-
-var g errgroup.Group
 
 func run(path string) error {
 	fmt.Println(color.BGPurple, "尝试", path, color.Reset)
 	// git pull
 	execpint.File(path, ".git", "git", "pull")
 	// pnpm i
-	g.Go(func() error {
-		execpint.File(path, "pnpm-lock.yaml", "pnpm", "i")
-		return nil
-	})
+	execpint.File(path, "pnpm-lock.yaml", "pnpm", "i")
 	// pnpm outdated
-	g.Go(func() error {
-		err := execpint.File(path, "pnpm-lock.yaml", "pnpm", "outdated")
-		if err != nil {
-			errPath = append(errPath, path)
-		}
-		return nil
-	})
-	g.Wait()
+	err := execpint.File(path, "pnpm-lock.yaml", "pnpm", "outdated")
+	if err != nil {
+		errPath = append(errPath, path)
+	}
 	return nil
 }
