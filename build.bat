8091bfc559d7ed9c94bf5bd9b641b5c03e7226a6
zichou <1303037989@qq.com>
2026-02-15 11:17:53 +0800

    android

--- build.bat
+++ build.bat
@@ -8,6 +8,10 @@ set GOOS=linux
 set GOARCH=arm64
 go build -trimpath -ldflags "-s -w" -o build/linux-arm64/ ./...
 
+set GOOS=android
+set GOARCH=arm64
+go build -trimpath -ldflags "-s -w" -o build/android-arm64/ ./...
+
 set GOOS=windows
 set GOARCH=amd64
 go build -trimpath -ldflags "-s -w" -o build/windows-amd64/ ./...
--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ export GOOS=linux
 export GOARCH=arm64
 go build -trimpath -ldflags "-s -w" -o build/linux-arm64/ ./...
 
+export GOOS=android
+export GOARCH=arm64
+go build -trimpath -ldflags "-s -w" -o build/android-arm64/ ./...
+
 export GOOS=windows
 export GOARCH=amd64
 go build -trimpath -ldflags "-s -w" -o build/windows-amd64/ ./...
