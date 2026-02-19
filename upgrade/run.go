3f129b5ba6661818e684ec3e65298c1848352eb9
zichouu <65216668+zichouu@users.noreply.github.com>
2026-02-20 00:37:34 +0800

    fix: git pull 错误时不执行后续步骤 (#4)

--- upgrade/run.go
+++ upgrade/run.go
@@ -12,15 +12,17 @@ func run(path string) {
 	fmt.Println(color.BgPurple, "尝试", path, color.Reset)
 	if slices.Contains(CanUseList, "git") {
 		// git pull
-		_ = exe.IfExist(path, ".git", []string{}, "git", "pull")
-	}
-	if slices.Contains(CanUseList, "pnpm") {
-		// pnpm i
-		_ = exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "i")
-		// pnpm outdated
-		err := exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "outdated")
-		if err != nil {
-			errPath = append(errPath, path)
+		err := exe.IfExist(path, ".git", []string{}, "git", "pull")
+		if err == nil {
+			if slices.Contains(CanUseList, "pnpm") {
+				// pnpm i
+				_ = exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "i")
+				// pnpm outdated
+				err := exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "outdated")
+				if err != nil {
+					errPath = append(errPath, path)
+				}
+			}
 		}
 	}
 }
