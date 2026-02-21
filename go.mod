33ff8ebc609eb9f2184df40a3265a36f75f999b2
zichouu <65216668+zichouu@users.noreply.github.com>
2026-02-21 10:12:45 +0800

    fix: 判断是否为 outdated (#10)
    * update: pkg 0.5.0
    
    * fix: 判断是否为 outdated

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.26.0
 
 require (
 	github.com/charmbracelet/huh v0.8.0
-	github.com/zichouu/go-pkg v0.3.2
+	github.com/zichouu/go-pkg v0.5.0
 	golang.org/x/sync v0.19.0
 )
 
--- upgrade/run.go
+++ upgrade/run.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"slices"
+	"strings"
 
 	"github.com/zichouu/go-pkg/color"
 	"github.com/zichouu/go-pkg/exe"
@@ -12,14 +13,18 @@ func run(path string) {
 	fmt.Println(color.BgPurple, "尝试", path, color.Reset)
 	if slices.Contains(CanUseList, "git") {
 		// git pull
-		err := exe.IfExist(path, ".git", []string{}, "git", "pull")
+		_, err := exe.IfExist(path, ".git", []string{}, "git", "pull")
 		if err == nil {
 			if slices.Contains(CanUseList, "pnpm") {
 				// pnpm i
-				_ = exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "i")
+				_, _ = exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "i")
 				// pnpm outdated
-				err := exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "outdated")
-				if err != nil {
+				out, err := exe.IfExist(path, "pnpm-lock.yaml", []string{}, "pnpm", "outdated")
+				outString := string(out)
+				isOutDated := strings.Contains(outString, "Package") &&
+					strings.Contains(outString, "Current") &&
+					strings.Contains(outString, "Latest")
+				if err != nil && isOutDated {
 					errPath = append(errPath, path)
 				}
 			}
